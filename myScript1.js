72d8d2031985c9b67803f143920fc531335c20e2
calvinoea <calvinebunamu@gmail.com>
2019-12-03 06:12:40 +0100

    Tough Rabbit Landing Page

---
+++ myScript1.js
@@ -0,0 +1,15 @@
+// Open Get Ride Page
+function openWin() {
+    window.open("http://127.0.0.1:5500/get-ride.html")
+}
+
+// Open Home Page
+
+function openHome() {
+    window.open("http://127.0.0.1:5500/index.html")
+}
+
+// Open Give Ride
+function openGive() {
+    window.open("http://127.0.0.1:5500/give-ride.html")
+}
